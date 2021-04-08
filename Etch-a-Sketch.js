44de33d4ecda37ef0b81f4ba7a2e0b2c258e68b3
jbuto454 <jakebuto@pm.me>
2021-04-07 22:40:44 -0400

    working on creating a css grid in javascript

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('#container');
+const div = document.createElement('div');
+container.appendChild(div);
+div.setAttribute('id', '1'); 
+
+container.style.grid = "250px / auto auto";
+container.style.gridTemplateRows="50px 50px";
+container.style.gridTemplateColumns="50px 50px";
+
+
+
+
