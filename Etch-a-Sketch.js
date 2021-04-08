78ca2c69808ad59c583e0c6be750f98c5155f865
jbuto454 <jakebuto@pm.me>
2021-04-07 23:22:33 -0400

    building out grid

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,11 +1,15 @@
 const container = document.querySelector('#container');
+
 const div = document.createElement('div');
 container.appendChild(div);
 div.setAttribute('id', '1'); 
 
-container.style.grid = "250px / auto auto";
-container.style.gridTemplateRows="50px 50px";
-container.style.gridTemplateColumns="50px 50px";
+
+container.style.display = 'grid';
+container.style.gridTemplateRows= "25px 25px 25px 25px";
+container.style.gridTemplateColumns= "25px 25px 25px 25px";
+
+
 
 
 
