db3ba6ef30f06f29d99558e6a985e31ac5971d8f
Jake <jakebuto@pm.me>
2021-04-11 14:49:45 -0400

    mvp version done

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -2,32 +2,59 @@ const container = document.querySelector('#container');
 
 //Creates a number(i) of div elements 
 //inside of the parent container
-function createDivs() {
-    for (i=0;i<16;i++) {
+function createDivs(n) {
+    for (i=0;i<n;i++) {
         const divi = document.createElement('div');
         container.appendChild(divi);
         divi.setAttribute('id', "d" + i); 
+        divi.setAttribute('class', 'changeColor');
         divi.style.cssText = "background-color: black";
     }
 }
 
-createDivs();
-
 //Creates a grid in the container element
-container.style.display = 'grid';
-container.style.gridTemplateRows= "100px 100px 100px 100px";
-container.style.gridTemplateColumns= "100px 100px 100px 100px";
+  container.style.display = 'grid';
 
-//Changes the color of one div when you hover over it
-let divw = document.querySelector('#d1');
+function gridRows(n) {
+  container.style.gridTemplateRows= `repeat(${n}, 1fr)`;
+  container.style.gridTemplateColumns= `repeat(${n}, 1fr)`;
+}
 
-divw.addEventListener('mouseover', function(){
-    divw.style.cssText = "background-color: blue";
-  })
+gridRows(20);
 
-divw.addEventListener('mouseout', function(){
-    divw.style.cssText = "background-color: pink";
-  })
+createDivs(400);
+
+
+//Changes the color of a div in the grid when you hover over it
+const divColor = document.querySelectorAll('div.changeColor');
+
+divColor.forEach(colorDivs);
+
+function colorDivs(div) {
+  div.addEventListener('mouseover', function(){
+    div.style.cssText = "background-color: blue";
+      })
+      
+  div.addEventListener('mouseout', function(){
+    div.style.cssText = "background-color: pink";
+      })
+}
+
+//changes the color of the divs in the grid back to black
+function eraseDivs(div) {
+    div.style.cssText = "background-color: black";
+}
+
+function erase() {
+  divColor.forEach(eraseDivs);
+}
+
+const erasebtn = document.querySelector("#erase");
+erasebtn.addEventListener('click', function() {erase()});
+
+
+
+
 
 
 
